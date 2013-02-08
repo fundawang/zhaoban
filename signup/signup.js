c2dea053097e5fecb37a0fa0b0bfb27f5f501c1f
Funda Wang <fundawang@gmail.com>
2013-02-09 03:48:39 +0800

    put site wide js into essential module

--- signup/signup.js
+++ signup/signup.js
@@ -48,32 +48,3 @@ jQuery.cookie = function(name, value, options) {
         return cookieValue;
     }
 };
-
-function deal_input_palceholder() {
-jQuery(function($){
-    if (!$.support.placeholder) {
-        $('input[placeholder], textarea[placeholder]').each(function(i, el){
-            el = $(el);
-            var defValue = el.attr('placeholder'), defColor = el.css('color');
-            el.focus(function(){
-                if (this.value === '' || this.value === defValue) {
-                    $(this).css('color', defColor);
-                    this.value = '';
-                }
-            })
-            .blur(blurHandler)
-            .closest('form').submit(function(){
-                if (el.val() === defValue) {
-                    el.val('');
-                }
-            });
-            blurHandler.call(el[0]);
-            function blurHandler(){
-                if (this.value === '' || this.value === defValue) {
-                    $(this).css('color', '#999');
-                    this.value = defValue;
-                }
-            }
-        });
-    }
-});}
